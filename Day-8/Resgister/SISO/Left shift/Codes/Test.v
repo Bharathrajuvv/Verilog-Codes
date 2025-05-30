2fc67f4d8b7b9c188f7fd4345f6853ba5fc636f0
Bharathrajuvv <rajubharath542@gmail.com>
2025-05-30 12:20:07 +0530

    Create Test.v

---
+++ Day-8/Resgister/SISO/Left shift/Codes/Test.v
@@ -0,0 +1,31 @@
+// Test Bench code for SISO Left Shift
+module test;
+  reg clk,rst,si;
+  wire so;
+  siso_sfr duv(clk,rst,si,so);
+  initial begin
+   
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    $monitor("time=%0t si=%b clk=%b rst=%b so=%b",$time,si,clk,rst,so);
+     clk=1;
+    forever #5 clk=~clk;
+  end
+  initial begin
+    rst=1;
+    #5 si=0;
+    rst=0;
+    si=1;
+    
+    si=1; #5;
+    si=1; #5;
+    si=1; #5;
+    si=1; #5;
+    si=0; #5;
+    si=0; #5;
+    si=0; #5;
+    si=1; #5;
+   #200 $finish;
+  end
+endmodule
+
