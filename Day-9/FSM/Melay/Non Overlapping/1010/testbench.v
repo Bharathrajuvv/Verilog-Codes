868e53b9873d2b0ffeca74e46cc4bab7de35ac04
Bharathrajuvv <rajubharath542@gmail.com>
2025-06-03 10:13:19 +0530

    Create testbench.v

---
+++ Day-9/FSM/Melay/Non Overlapping/1010/testbench.v
@@ -0,0 +1,26 @@
+// Testbench code for Sequence dector in 1010 using Melay Non-overlapping
+module test;
+  reg in,clk,rst;
+  wire ou;
+  seq_det duv(in,clk,rst,ou);
+  initial clk=0;
+  always #5 clk=~clk;
+  initial begin 
+    $dumpfile("test.vcd");
+    $dumpvars;
+    $monitor("time=%0t in=%b clk=%b rst=%b ou=%b",$time,in,clk,rst,ou);
+    rst=1;
+    #5 in=0;
+    rst=0;
+    #5 in=1;
+    #5 in=0;
+    #5 in=1;
+    #5 in=0;
+    #5 in=1;
+    #5 in=0;
+    #5 in=1;
+    #5 in=0;
+    #5 in=1;
+    #200 $finish;
+  end
+endmodule
