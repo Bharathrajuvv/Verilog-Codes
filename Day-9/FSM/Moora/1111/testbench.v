af1acb75e4e3c69cba11712491996e91e3c8457d
Bharathrajuvv <rajubharath542@gmail.com>
2025-06-03 10:46:37 +0530

    Update testbench.v

--- Day-9/FSM/Moora/1111/testbench.v
+++ Day-9/FSM/Moora/1111/testbench.v
@@ -1,1 +1,25 @@
+// Testbench code for sequence dector for 1111 usgin Moore Machine in Non-overlapping
+module test;
+  reg in,clk,rst;
+  wire ou;
+  seq_det duv(in,clk,rst,ou);
+  initial clk=0;
+  always #5 clk =!clk;
+  initial begin
+    $dumpfile("test.vcd");
+    $dumpvars;
+    $monitor("time=%0t in=%b clk=%b rst=%b ou=%b",$time,in,clk,rst,ou);
+    rst=1;
+    #5 in=0;
+    rst=0;
+    #5 in=1;
+    #5 in=0;
+    #5 in=1;
+    #5 in=1;
+    #5 in=1;
+    #5 in=1;
+    #5 in=0;
+    #200 $finish;
+  end
+endmodule
 
