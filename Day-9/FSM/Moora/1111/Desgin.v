1b2ee7dc070b5ef86e4b780f138ef86db513ffd6
Bharathrajuvv <rajubharath542@gmail.com>
2025-06-03 10:46:01 +0530

    Update Desgin.v

--- Day-9/FSM/Moora/1111/Desgin.v
+++ Day-9/FSM/Moora/1111/Desgin.v
@@ -1,1 +1,27 @@
-
+// Desgin code for sequence dector for 1111 usgin Moore Machine in Non-overlapping
+module seq_det(input in,clk,rst, output ou);
+  parameter a=4'd1;
+  parameter b=4'd2;
+  parameter c=4'd3;
+  parameter d=4'd4;
+  parameter e=4'd5;
+  reg[4:0] st,nx_st;
+  always@(posedge clk or negedge rst)begin
+    if(!rst)begin
+      st<=a;
+    end
+    else
+      st<=nx_st;
+  end
+  always@(*)begin
+    case(st)
+      a:nx_st = in?b:a;
+      b:nx_st = in?c:a;
+      c:nx_st = in?d:a;
+      d:nx_st = in?e:a;
+      d:nx_st = in?a:a;
+      default:nx_st=a;
+    endcase
+  end
+  assign ou=(st==a)&&(in==1);
+endmodule
