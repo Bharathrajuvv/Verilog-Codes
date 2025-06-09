f90d6307f284cabe415e3e0e39db62fb454c6bc0
Bharathrajuvv <rajubharath542@gmail.com>
2025-06-09 12:30:11 +0530

    Update Testbench.v

--- Day-11/Memory/Static Memory/Single Port Memory/Testbench.v
+++ Day-11/Memory/Static Memory/Single Port Memory/Testbench.v
@@ -1,40 +1,48 @@
 // Testbench code for Single Port Static RAM Memory
-module test;
-  reg clk,rst,wr;
-  reg [2:0] addr;
-  reg [3:0] x;
-  wire [3:0] y;
-  
-  sin_ram duv(clk,rst,wr,addr,x,y);
-  initial clk=0;
-  always #5 clk=~clk;
-  initial begin
-    $dumpfile("test.vcd");
-    $dumpvars;
-    $monitor("time=%0t clk=%b rst=%b wr=%b addr=%b x=%b y=%b",$time,clk,rst,addr,x,y);
-    rst=1;
-    #2 rst=0;
-    wr=1;
-        addr=3'b000; x=4'b0111; #10;
-        addr=3'b001; x=4'b1110; #10;
-        addr=3'b010; x=4'b0010; #10;
-        addr=3'b011; x=4'b1100; #10;
-        addr=3'b100; x=4'b0011; #10;
-        addr=3'b101; x=4'b1010; #10;
-        addr=3'b110; x=4'b0111; #10;
-        addr=3'b111; x=4'b0101; #10;
-    wr=0;
-        addr=3'b000; #10;
-        addr=3'b001; #10;
-        addr=3'b010; #10;
-        addr=3'b011; #10;
-        addr=3'b100; #10;
-        addr=3'b101; #10;
-        addr=3'b110; #10;
-        addr=3'b111; #10;
-    #200 $finish;
-  end
-endmodule
+module mem_tb() ;
+	reg clk,rst,w_r ;
+	reg [3:0] din ;
+	reg [2:0] addr ;
+	wire [3:0] dout ;
+	
+	mem dut (clk,rst,w_r,din,addr,dout);
+	
+	initial begin
+		clk=0 ;
+		forever #10 clk=~clk ;
+	end 
+	
+	initial begin
+		$dumpfile("dump.vcd");
+		$dumpvars(0,dut) ;
+		$monitor("Time=%0t clk=%b rst=%b w_r=%b din=%b addr=%b dout=%b",$time,clk,rst,w_r,din,addr,dout ) ;
+		
+		#0  rst=1 ;
+		#30 rst=0 ;
+		
+		w_r =1 ;  
+		
+		addr=3'b000 ; din=4'b1111 ; #20 ;
+		addr=3'b001 ; din=4'b1110 ; #20 ;
+		addr=3'b010 ; din=4'b1101 ; #20 ;
+		addr=3'b011 ; din=4'b1100 ; #20 ;
+		addr=3'b100 ; din=4'b1011 ; #20 ;
+		addr=3'b101 ; din=4'b0101 ; #20 ;
+		addr=3'b110 ; din=4'b1001 ; #20 ;
+		addr=3'b111 ; din=4'b1000 ; #20 ;
+		
+		w_r=0 ;    
+		
+		addr=3'b000 ; #20 ;
+		addr=3'b001 ; #20 ;
+		addr=3'b010 ; #20 ;
+		addr=3'b011 ; #20 ;
+		addr=3'b100 ; #20 ;
+		addr=3'b101 ; #20 ;
+		addr=3'b110 ; #20 ;
+		addr=3'b111 ; #20 ;
+		
+		$finish() ; 
+	end 
+endmodule 
 
-    
-    
